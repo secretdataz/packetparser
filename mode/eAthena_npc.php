57c86030bbd612cd5b3f745bb05730aea7702437
Yommy <Yommy@f033bea6-de0a-43be-8f3b-be0617f6e26f>
2012-02-01 13:25:08 +0000

    * added basic npc capturing mode

---
+++ mode/eAthena_npc.php
@@ -0,0 +1,85 @@
+<?php
+
+// class npc_ripper extends parser {
+	// public $npc_list = array();
+	// public $data = array();
+	// public $menu_list = array();
+// }
+
+// packet_parser functions
+function PP_MODE_INIT($parser) {
+	//global $npc_list = array();
+	//global $data = array();
+	$parser->mode["mode_name"] = "eAthena_npc";	//
+	$parser->mode["extra_bytes"] = false;		// warning about extra packet data
+	$parser->mode["debug"] = true;				// write packets to file
+	$parser->mode["save_npc"] = true;
+	
+	if($parser->mode["debug"]) {
+		$debug_filename = "debug/".date("Ymd-gis").".txt";
+		$parser->debug = fopen($debug_filename, "w");
+	}
+	if($parser->mode["save_npc"]) {
+		$npc_filename = "npc_capture/".date("Ymd-gis").".txt";
+		$parser->npc_file = fopen($npc_filename, "w");
+	}
+}
+
+function echo_save($parser, $text){
+	echo $text;
+	fwrite($parser->npc_file, $text);
+}
+
+function PACKET_HC_NOTIFY_ZONESVR($parser) {
+	$parser->data['map'] = $parser->string(16, 6);
+	$parser->data['talking_to_npc'] = false;
+}
+
+function PACKET_ZC_SAY_DIALOG($parser){
+    $gid = $parser->long(4);
+    if($parser->data['talking_to_npc'] == false){
+        $parser->data['talking_to_npc'] = true;
+        echo_save($parser,"\n\n".$parser->npc_list[$gid]['map'].",".$parser->npc_list[$gid]['x'].",".$parser->npc_list[$gid]['y'].","."4"."\tscript\t".$parser->npc_list[$gid]['name']."\t".$parser->npc_list[$gid]['job'].",{\n");
+        echo_save($parser,"OnClick:\n");
+    }
+    echo_save($parser,"\tmes \"" .$parser->string($parser->word(2)-8,8) ."\";\n");
+}
+
+function PACKET_ZC_WAIT_DIALOG($parser){
+    echo_save($parser,"\tnext;\n");
+}
+
+function PACKET_ZC_CLOSE_DIALOG($parser){
+    echo_save($parser,"\tclose2;\n");
+    echo_save($parser,"end;\n");
+    $parser->data['talking_to_npc'] = false;
+}
+
+function PACKET_ZC_MENU_LIST($parser){
+
+	$select = $parser->string($parser->word(2)-8,8);
+	$parser->menu_list = explode(":",":".$select); // begin with : to create a blank entry at begining
+	$parser->menu_list[255] = "cancel clicked";
+	echo_save($parser,"\tswitch(select(\"$select\") {\n");
+}
+
+function PACKET_CZ_CHOOSE_MENU($parser){
+	$chose = $parser->byte(6);
+	$option = $parser->menu_list[$chose];
+	echo_save($parser,"\tcase $chose: // $option\n");
+}
+
+function PACKET_ZC_NOTIFY_STANDENTRY6($parser){
+    $gid = $parser->long(5);
+    $parser->npc_list[$gid]['GID'] = $parser->long(5);
+    $parser->npc_list[$gid]['job'] = $parser->word(19);
+    $parser->npc_list[$gid]['name'] = $parser->string(24,65);
+    $parser->npc_list[$gid]['map'] = $parser->data['map'];
+    list($x,$y) = explode(",",$parser->xy(55));
+    $parser->npc_list[$gid]['x'] = $x;
+    $parser->npc_list[$gid]['y'] = $y;
+    echo "Seen NPC # ".$parser->npc_list[$gid]['name'] ." \n";
+	//print_r($parser->npc_list[$gid]);
+}
+
+?>
--- readme.txt
+++ readme.txt
@@ -12,35 +12,35 @@ Packet_Parser receives packet data from the Forwarder, and outputs data from eac
 
 Alternative to the Forwarder, the ro client can be patched to load wpp.dll
 
+Packet_Parser requires a correct packet length table (plen_*) to work correctly
+
 TODO:
-* move remaining output into full_info.php
-* add a mode selection at startup
-* add a packet length file selection at startup
 * create a mode specific to capturing npc data, and output into athena script format <3
 * update plen_extract to support new clients
 
 
 Packet_Parser
 |   packet_forwarding.exe	-- listens for ragnarok packets on network, and forwards to parser
-|   parser.bat			-- Run this to start the parser
-|   parser.php			-- main script, listens for socket data from forwarder / wpp.dll, and pushes to extracter
-|   parser_cls.php		-- Core Parser file, resposible for extracting packet data
-|   readme.txt			-- This file
+|   parser.bat				-- Run this to start the parser
+|   parser.php				-- main script, listens for socket data from forwarder / wpp.dll, and pushes to extracter
+|   parser_cls.php			-- Core Parser file, resposible for extracting packet data
+|   readme.txt				-- This file
 +---data
 |   +---enum
 |   |       efst.txt		-- Status Effects
 |   |       item.txt		-- Items
 |   |       jobtype.txt		-- Jobs / Npc / Mobs / Homunc / Mercenary
 |   |       skill.txt		-- Skills
-|   |       var.txt		-- Var
+|   |       var.txt			-- Var
 |   \---packet
 |           func.txt		-- Function list table
-|           plen.txt		-- Packet length table
+|           plen_*.txt		-- Packet length table
 +---dev
-|   +---dll_inject		-- wpp.dll and script to make client load this dll
+|   +---dll_inject			-- wpp.dll and script to make client load this dll
 |   +---packet_capture		-- Source of Packet_Forwarder, using libPcap
 |   +---plen_extract		-- Extract packet length table from ro client (only vc9 linked)
-|   \---structure		-- A script to convert "extracted aegis packet structure defines" into full_mode.php
+|   \---structure			-- A script to convert "extracted aegis packet structure defines" into full_mode.php
 |   
 \---mode
         full_info.php		-- prints out all known packet data
+		*.php				-- other modes
